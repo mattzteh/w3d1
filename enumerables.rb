28772956078afaef098af8ba531c0bd6b2908fdd
mattzteh <mattzteh@gmail.com>
2022-06-27 15:30:43 -0700

    3rd Turn

--- enumerables.rb
+++ enumerables.rb
@@ -43,7 +43,7 @@ class Array
     def my_flatten
         flattened = []
         self.each do  |ele| 
-            if ele.kind_of?Array
+            if ele.kind_of?(Array)
                 flattened += ele.my_flatten
             else
                 flattened << ele
@@ -52,17 +52,37 @@ class Array
         flattened
     end
 
+    def my_zip(*arrs)
+        zipped = []
+        (0...self.length).each do |i|
+            subarr = [self[i]]
+            arrs.each do |arr|
+                subarr << arr[i]
+            end
+            zipped << subarr
+        end
+        zipped
+    end
+
 
 end
     
-
-
 # a = [1, 2, 3]
 # p a.my_any? { |num| num > 1 } # => true
 # p a.my_any? { |num| num == 4 } # => false
 # p a.my_all? { |num| num > 1 } # => false
 # p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+
 
 
